2d72e5d5a5da0f03903ad8e03eaedca0ac2afc2c
lingeorge88 <lingeorge04@gmail.com>
2023-05-03 20:10:36 -0700

    added CRUD routes for users

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -29,6 +29,56 @@ const userController = {
         }
     },
 
+    async createUser(req, res){
+        try {
+            const newUser = await User.create(req.body);
+            res.json(newUser);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
+
+    async updateUser(req, res){
+        try {
+            const user = await User.findOneAndUpdate( { _id: req.params.id },
+            req.body, { new: true, runValidators: true});
+            if(!user){
+                res.status(404).json({ message: 'No user found with this id!' }); 
+            }
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    deleteUser({ params }, res) {
+        // delete the user
+        User.findOneAndDelete({ _id: params.id })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id'});
+                return;
+            }
+            // remove the user from any friends arrays
+            User.updateMany(
+                { _id : {$in: dbUserData.friends } },
+                { $pull: { friends: params.id } }
+            )
+            .then(() => {
+                // remove any comments from this user
+                Thought.deleteMany({ username : dbUserData.username })
+                .then(() => {
+                    res.json({message: "Successfully deleted user"});
+                })
+                .catch(err => res.status(400).json(err));
+            })
+            .catch(err => res.status(400).json(err));
+        })
+        .catch(err => res.status(400).json(err));
+    },
+
+
 }
 
 
--- models/User.js
+++ models/User.js
@@ -3,8 +3,8 @@ const { Schema, model } = require('mongoose');
 const userSchema = new Schema({
     username: {type: String, required: true, trim: true, unique: true},
     email: {type: String, required: true, unique:true, match: [/.+@.+\..+/]},
-    thoughts: [{type: mongoose.Schema.Types.ObjectId, ref:'Thought'}],
-    friends: [{type: mongoose.Schema.Types.ObjectId, ref:'User'}],
+    thoughts: [{type: Schema.Types.ObjectId, ref:'Thought'}],
+    friends: [{type: Schema.Types.ObjectId, ref:'User'}],
 },
     {toJSON: {
         virtuals: true
